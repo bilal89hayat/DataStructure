2665555448ac0ef37860763c234c44300d957c26
Bilal Hayat <bilal89hayat>
2024-07-10 18:47:42 +0530

    traversal

--- src/Tree/InOrderTraversal.java
+++ src/Tree/InOrderTraversal.java
@@ -1,4 +1,31 @@
 package Tree;
 
 public class InOrderTraversal {
+
+    public static void main(String[] args) {
+
+        Node node = new Node(5);
+        node.left = new Node(10);
+        node.right = new Node(15);
+        node.left.left = new Node(20);
+        node.left.right = new Node(25);
+        node.right.left = new Node(30);
+        node.right.right = new Node(35);
+        node.right.right.right = new Node(60);
+        node.right.right.right.right = new Node(70);
+        node.left.right.right = new Node(45);
+
+        inOrder(node);
+    }
+
+    private static void inOrder(Node node) {
+
+        if(node == null){
+           return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.val);
+        inOrder(node.right);
+    }
 }
--- src/Tree/PostOrderTraversal.java
+++ src/Tree/PostOrderTraversal.java
@@ -1,4 +1,31 @@
 package Tree;
 
 public class PostOrderTraversal {
+
+    public static void main(String[] args) {
+
+        Node node = new Node(5);
+        node.left = new Node(10);
+        node.right = new Node(15);
+        node.left.left = new Node(20);
+        node.left.right = new Node(25);
+        node.right.left = new Node(30);
+        node.right.right = new Node(35);
+        node.right.right.right = new Node(60);
+        node.right.right.right.right = new Node(70);
+        node.left.right.right = new Node(45);
+
+        postOrder(node);
+    }
+
+    private static void postOrder(Node node) {
+
+        if (node == null){
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.val);
+    }
 }
